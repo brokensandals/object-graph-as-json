8c24afed5c067e9482dcb8e808ea7d0e00390cbd
Jacob Williams <jacobaw@gmail.com>
2020-01-14 20:45:35 -0800

    encode symbols

--- src/Encoder.js
+++ src/Encoder.js
@@ -1,18 +1,67 @@
+const builtins = new Map();
+builtins.set(Infinity, 'Infinity');
+// Well-known symbols - https://tc39.es/ecma262/#sec-well-known-symbols
+const wellKnownSymbols = [
+  Symbol.asyncIterator,
+  Symbol.hasInstance,
+  Symbol.isConcatSpreadable,
+  Symbol.iterator,
+  Symbol.match,
+  Symbol.matchAll,
+  Symbol.replace,
+  Symbol.search,
+  Symbol.species,
+  Symbol.split,
+  Symbol.toPrimitive,
+  Symbol.toStringTag,
+  Symbol.unscopables,
+]
+for (const symbol of wellKnownSymbols) {
+  builtins.set(symbol, symbol.description);
+}
+
 export class Encoder {
   constructor() {
-    this.ids = new WeakMap();
+    this.objectIds = new WeakMap();
+    this.symbolIds = new Map();
+    this.nextId = 1;
+  }
+
+  internObject(value) {
+    let id = this.objectIds.get(value);
+    
+    if (!id) {
+      id = this.nextId++;
+      this.objectIds.set(value, id);
+    }
+
+    return id;
+  }
+
+  internSymbol(symbol) {
+    let id = this.symbolIds.get(symbol);
+
+    if (!id) {
+      id = this.nextId++;
+      this.symbolIds.set(symbol, id);
+    }
+
+    return id;
   }
 
   encode(value) {
-    let refs = null;
+    let refIds = null;
 
     const recurse = value => {
       if (value === null) {
         return null;
       } else if (Object.is(NaN, value)) {
-        return { 'type': 'builtin', 'name': 'NaN' };
-      } else if (Object.is(Infinity, value)) {
-        return { 'type': 'builtin', 'name': 'Infinity' };
+        return { type: 'builtin', name: 'NaN' };
+      }
+
+      const builtinName = builtins.get(value);
+      if (builtinName) {
+        return { type: 'builtin', name: builtinName };
       }
 
       const type = typeof value;
@@ -28,8 +77,17 @@ export class Encoder {
         case 'string':
           return value;
         case 'symbol':
-          // TODO
-          break;
+          const id = this.internSymbol(value);
+          
+          if (!refIds) {
+            refIds = new Set();
+          }
+          if (refIds.has(id)) {
+            return { type: 'ref', id };
+          }
+
+          refIds.add(id);
+          return { type, id, description: value.description };
         case 'function':
           // TODO
           break;
--- src/Encoder.test.js
+++ src/Encoder.test.js
@@ -19,13 +19,31 @@ describe('Encoder', () => {
 
   describe('builtins', () => {
     const tests = [
+      // Number-related
       [NaN, 'NaN'],
       [Infinity, 'Infinity'],
+
+      // undefined
       [undefined, 'undefined'],
+      
+      // Well-known symbols - https://tc39.es/ecma262/#sec-well-known-symbols
+      [Symbol.asyncIterator, 'Symbol.asyncIterator'],
+      [Symbol.hasInstance, 'Symbol.hasInstance'],
+      [Symbol.isConcatSpreadable, 'Symbol.isConcatSpreadable'],
+      [Symbol.iterator, 'Symbol.iterator'],
+      [Symbol.match, 'Symbol.match'],
+      [Symbol.matchAll, 'Symbol.matchAll'],
+      [Symbol.replace, 'Symbol.replace'],
+      [Symbol.search, 'Symbol.search'],
+      [Symbol.species, 'Symbol.species'],
+      [Symbol.split, 'Symbol.split'],
+      [Symbol.toPrimitive, 'Symbol.toPrimitive'],
+      [Symbol.toStringTag, 'Symbol.toStringTag'],
+      [Symbol.unscopables, 'Symbol.unscopables'],
     ];
 
     for (const [value, expected] of tests) {
-      test(`${value} encodes correctly`, () => {
+      test(`${typeof value === 'symbol' ? value.description : value} encodes correctly`, () => {
         expect(encoder.encode(value)).toEqual({ type: 'builtin', name: expected });
       });
     }
@@ -35,6 +53,27 @@ describe('Encoder', () => {
     expect(encoder.encode(BigInt('123456789012345678901234567890'))).toEqual({
       type: 'bigint',
       string: '123456789012345678901234567890',
+    });
+  });
+
+  describe('Symbols', () => {
+    test('encode correctly', () => {
+      expect(encoder.encode(Symbol('meep'))).toEqual({
+        type: 'symbol',
+        id: 1,
+        description: 'meep',
+      });
+    });
+
+    test('reuse IDs across calls', () => {
+      const sym1 = Symbol('meep');
+      const sym2 = Symbol('meep');
+      const expected1 = { type: 'symbol', id: 1, description: 'meep' };
+      const expected2 = { ...expected1, id: 2 };
+      expect(encoder.encode(sym1)).toEqual(expected1);
+      expect(encoder.encode(sym1)).toEqual(expected1);
+      expect(encoder.encode(sym2)).toEqual(expected2);
+      expect(encoder.encode(sym1)).toEqual(expected1);
     });
   });
 });
