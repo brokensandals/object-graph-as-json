b0c9612562cce3f0e6c0c4d30d260f9a5fc07eeb
Jacob Williams <jacobaw@gmail.com>
2020-01-16 22:04:34 -0800

    rework builds

--- scripts/build-embed-json.js
+++ scripts/build-embed-json.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const fs = require('fs');
 
-const Encoder = fs.readFileSync(path.resolve(__dirname, '..', 'dist', 'object-graph-as-json.Encoder.min.js'), 'utf8');
-const UnsafeDecoder = fs.readFileSync(path.resolve(__dirname, '..', 'dist', 'object-graph-as-json.UnsafeDecoder.min.js'), 'utf8');
+const embedDir = path.resolve(__dirname, '..', 'target', 'embed');
+const Encoder = fs.readFileSync(path.resolve(embedDir, 'Encoder.min.js'), 'utf8');
+const UnsafeDecoder = fs.readFileSync(path.resolve(embedDir, 'UnsafeDecoder.min.js'), 'utf8');
 
-const genSrcDir = path.resolve(__dirname, '..', 'gen-src');
-fs.mkdirSync(genSrcDir);
+const genSrcDir = path.resolve(__dirname, '..', 'target', 'gen-src');
+if (!fs.existsSync(genSrcDir)) {
+  fs.mkdirSync(genSrcDir);
+}
 
 const json = JSON.stringify({ Encoder, UnsafeDecoder }, null, 2);
 const source = 'export default ' + json + ';';
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,28 @@
 const path = require('path');
 
-module.exports = {
-  entry: {
-    Encoder: './src/Encoder.js',
-    UnsafeDecoder: './src/UnsafeDecoder.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'object-graph-as-json.[name].min.js',
-    library: ['OGAJ', '[name]'],
-    libraryTarget: 'var',
-  },
+module.exports = (env) => {
+  if (env && env.embed) {
+    return {
+      entry: {
+        Encoder: './src/Encoder.js',
+        UnsafeDecoder: './src/UnsafeDecoder.js',
+      },
+      output: {
+        path: path.resolve(__dirname, 'target', 'embed'),
+        filename: '[name].min.js',
+        library: '[name]',
+        libraryTarget: 'var',
+      },
+    };
+  }
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'target', 'umd'),
+      filename: 'object-graph-as-json.min.js',
+      library: ['OGAJ'],
+      libraryTarget: 'umd',
+    },
+  };
 };
